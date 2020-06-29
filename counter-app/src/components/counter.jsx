39ee31a72a4b882dc33d19db1d661aa2f40dbf4d
rahulAgrBej <rahul.agrawal.b@gmail.com>
2020-06-29 17:18:44 -0400

    workign with styling

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -5,11 +5,16 @@ class Counter extends Component {
         count: 0,
         imageUrl: 'https://picsum.photos/200'
     };
+
+    styles = {
+        fontSize: '20px',
+        fontWeight: "bold"
+    };
     render() { 
         return (
             <React.Fragment>
-                <span className="badge badge-primary">{this.formatCount()}</span>
-                <button>Increment</button>
+                <span style={this.styles} className="badge badge-primary m-2">{this.formatCount()}</span>
+                <button className="btn btn-seconday btn-sm">Increment</button>
             </React.Fragment>
         );
     }
