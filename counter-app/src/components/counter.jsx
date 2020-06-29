83344da256521d13b0490f91a238e09201ac7c19
rahulAgrBej <rahul.agrawal.b@gmail.com>
2020-06-29 17:14:45 -0400

    workign with state and formating based on values

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-    state = {  }
+    state = {
+        count: 0,
+        imageUrl: 'https://picsum.photos/200'
+    };
     render() { 
         return (
             <React.Fragment>
-                <h1>Hello World!</h1>
+                <span className="badge badge-primary">{this.formatCount()}</span>
                 <button>Increment</button>
             </React.Fragment>
         );
+    }
+
+    formatCount() {
+        return this.state.count === 0 ? 'Zero' : this.state.count;
     }
 }
  
