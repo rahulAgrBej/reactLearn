150c91b924437f46aa43c2d3a9ead869708796fa
rahulAgrBej <rahul.agrawal.b@gmail.com>
2020-06-29 23:17:34 -0400

    now all state is maintained in counter list single source of truth

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -1,40 +1,28 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-    state = {
-        value: this.props.value
-    };
 
-    constructor(props) {
-        super(props);
-        this.handleIncrement = this.handleIncrement.bind(this);
-    }
 
-    handleIncrement() {
-        this.setState(
-            {value: this.state.value + 1}
-            );
-    }
 
     render() {
         
         return (
             <React.Fragment>
                 <span className={ this.getBadgeClasses() }>{this.formatCount()}</span>
-                <button onClick={ this.handleIncrement } className="btn btn-seconday btn-sm">Increment</button>
-                <button onClick={this.props.onDelete} className="btn btn-danger btn-sm m-2">Delete</button>
+                <button onClick={() => this.props.onIncrement(this.props.counterObj)} className="btn btn-seconday btn-sm">Increment</button>
+                <button onClick={() => this.props.onDelete(this.props.counterObj.id)} className="btn btn-danger btn-sm m-2">Delete</button>
                 <br/>
             </React.Fragment>
         );
     }
 
     formatCount() {
-        return this.state.value === 0 ? 'Zero' : this.state.value;
+        return this.props.counterObj.value === 0 ? 'Zero' : this.props.counterObj.value;
     }
 
     getBadgeClasses() {
         let classes  ="badge m-2 badge-";
-        classes += (this.state.count === 0) ? "warning" : "primary";
+        classes += (this.props.counterObj.value === 0) ? "warning" : "primary";
         return classes;
     }
 }
--- counter-app/src/components/counters.jsx
+++ counter-app/src/components/counters.jsx
@@ -11,17 +11,55 @@ class CounterList extends Component {
         ]
     };
 
-    handleDelete() {
-        console.log("event handler called")
+    handleIncrement = (counter) => {
+        console.log(counter);
+        let newCounters = [...this.state.counters]
+        const index = this.state.counters.indexOf(counter);
+
+        newCounters[index] = {...counter};
+        newCounters[index].value += 1;
+        console.log('state', this.state.counters);
+        console.log('newCounters', newCounters);
+
+        this.setState({
+            counters: newCounters
+        });
+        
+    }
+
+    handleReset = () => {
+        const resetCounters = this.state.counters.map(c => {
+            c.value = 0;
+            return c;
+        });
+
+        this.setState({
+            counters: resetCounters
+        });
+
+    }
+
+    handleDelete = (counterId) => {
+        console.log("event handler called", counterId);
+        const newCounters = this.state.counters.filter(c => c.id !== counterId);
+        this.setState({
+            counters: newCounters
+        });
     };
 
     render() { 
         return (
             <React.Fragment>
+                <button onClick={this.handleReset} className="btn btn-primary btn-sm m-2">Reset</button><br />
                 {
                     this.state.counters.map(
                         counter => 
-                        <Counter key={counter.id} value={counter.value} id={counter.id} onDelete={this.handleDelete} />
+                        <Counter
+                            key={counter.id}
+                            onDelete={this.handleDelete}
+                            onIncrement={this.handleIncrement}
+                            counterObj = {counter}
+                        />
                     )
                 }
             </React.Fragment>
